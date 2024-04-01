5cdf7d1815eac356acefb63719cc9920f553b923
William Elder <curseyoumcduck@gmail.com>
2024-04-01 11:57:21 +1100

    Using Rust By Example to teach myself Rust is also a method by which I can refamiliarize myself with git. This is just a hello world, so it's just this one message.

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+// 1. HELLO WORLD demonstrates printing
+// It also presents an introductory example of comments
+
+fn main() {
+    // printing to standard out
+    println!("Hello, world!");
+    println!("I'm a Rustacean!");
+}
