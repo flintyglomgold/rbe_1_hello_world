6e26daa5bac6292df42f82c6e139052e09a7422f
William Elder <curseyoumcduck@gmail.com>
2024-04-08 12:21:41 +1000

    doing some FUNCTIONAL PROGRAMMING NONSENSE IN HERE oh look I'm just going to take the first element of this list and then pass the rest recursively???? HMMM? OH WILL YOU? madness. MADNESS they are playing us for absolute fools. Seriously though this is some cool stuff.

--- rbe_3_2_subsections/src/main.rs
+++ rbe_3_2_subsections/src/main.rs
@@ -4,6 +4,7 @@
 // 3.2.3 Testcase: linked-list
 // many enum shenanigans in one place -- how apropos
 #![allow(dead_code)]
+use crate::List::*;
 
 enum Status {
     Rich,
@@ -29,6 +30,58 @@ enum Color {
     Blue  = 0x0000ff,
 }
 
+enum List {
+    // Cons: Tuple struct that wraps an element and a pointer to the next node
+    Cons(u32, Box<List>),
+    // Nil: A node that signifies the end of the linked list
+    Nil,
+}
+
+// Methods can be attached to an enum
+impl List {
+    // Create an empty list
+    fn new() -> List {
+        Nil
+    }
+
+    // Consume a list and return the same list with a new element at its start
+    fn prepend(self, elem: u32) -> List {
+        Cons(elem, Box::new(self)) // cute
+    }
+
+    // Return the length of the list
+    fn len(&self) -> u32 {
+        // `self` has to be matched, because the behavior of this method
+        // depends on the variant of `self`
+        // `self` has type `&List`, and `*self` has type `List`, matching on a
+        // concrete type `T` is preferred over a match on a reference `&T`
+        // after Rust 2018 you can use self here and tail (with no ref) below as well,
+        // rust will infer &s and ref rail
+        // see https://doc.rust-lang.org/edition-guide/rust-2018/ownership-and-lifetimes/default-match-bindings.html
+        //
+        // did you get all that?
+        // me neither
+        match *self {
+            // can't take ownership of the fail, because `self` is borrowed;
+            // instead take a reference to the tail (unless you're using Rust 2018 or later)
+            Cons(_, ref tail) => 1 + tail.len(),
+            // Base Case: An empty list has zero length
+            Nil => 0
+        }
+    }
+
+    // Return representation of the list as a heap allocated string
+    fn stringify(&self) -> String {
+        match * self {
+            Cons(head, ref tail) =>{
+                format!("{}, {}", head, tail.stringify())
+            },
+            Nil => {
+                format!("Nil")
+            },
+        }
+    }
+}
 
 fn main() {
     // The `use` declaration can be used so manual scoping isn't needed:
@@ -56,5 +109,14 @@ fn main() {
 
     println!("roses are #{:06x}", Color::Red as i32);
     println!("violets are #{:06x}", Color::Blue as i32);
+
+    let mut list = List::new();
+
+    list = list.prepend(1);
+    list = list.prepend(2);
+    list = list.prepend(3);
+
+    println!("linked list has length: {}", list.len());
+    println!("{}", list.stringify());
 }
 
