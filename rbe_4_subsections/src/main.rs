85d25c500867fb1726832a63d73f32868cb3fb53
William Elder <curseyoumcduck@gmail.com>
2024-04-10 13:33:51 +1000

    Looks like declare first is techncially possible but let's agree not to do that. Also freezing is notable but I should learn more about the benefits of shadowing.

--- rbe_4_subsections/src/main.rs
+++ rbe_4_subsections/src/main.rs
@@ -48,4 +48,38 @@ fn main() {
     println!("outside inner block: {}", shadowed_binding);
     let shadowed_binding = 2;
     println!("shadowed in outer block: {}", shadowed_binding);
+
+    // DECLARE FIRST
+
+    let a_binding;
+
+    {
+        let x = 2;
+
+        // initializing the binding
+        a_binding = x * x;
+    }
+
+    println!("a binding: {}", a_binding);
+
+    let another_binding;
+
+    // println!("another binding: {}", another_binding);
+
+    another_binding = 1;
+
+    println!("another binding: {}", another_binding);
+
+    // FREEZING
+
+    let mut _mutable_integer = 7i32;
+
+    {
+        // mutable variable shadowed by immutable variable
+        let _mutable_integer = _mutable_integer;
+
+        // _mutable_integer = 50;
+    }
+
+    _mutable_integer = 3;
 }
