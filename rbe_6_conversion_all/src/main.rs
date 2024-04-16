eed13a819891510489fbaa5bc75670952f557a67
William Elder <curseyoumcduck@gmail.com>
2024-04-16 16:53:07 +1000

    whole lotta conversion happening here - a lot of syntax that I've got to get used to, but yet again I'm intrigued by what's here.

---
+++ rbe_6_conversion_all/src/main.rs
@@ -0,0 +1,88 @@
+use std::convert::From;
+use std::convert::Into;
+use std::convert::TryFrom;
+use std::convert::TryInto;
+use std::fmt;
+
+#[derive(Debug)]
+struct Number {
+    value: i32,
+}
+
+impl From<i32> for Number {
+    fn from(item: i32) -> Self {
+        Number { value: item }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct EvenNumber(i32);
+
+impl TryFrom<i32> for EvenNumber {
+    type Error = ();
+
+    fn try_from(value: i32) -> Result<Self, Self::Error> {
+        if value % 2 == 0 {
+            Ok(EvenNumber(value))
+        } else {
+            Err(())
+        }
+    }
+}
+
+/*
+impl Into<Number> for i32 {
+    fn into(self) -> Number {
+        Number { value: self }
+    }
+}
+*/
+
+// The real lesson is that if you implement From
+// You get into for free
+// This is commented out because I get a compiler warning
+// that defining this means *conflicting definitions* of
+// the Into trait
+
+// so that's actually very cool
+
+struct Circle {
+    radius: i32
+}
+
+impl fmt::Display for Circle {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Circle of radius {}", self.radius)
+    }
+}
+
+fn main() {
+    let num = Number::from(50);
+    println!("My number is {:?}", num);
+
+    let int = 5;
+
+    let num: Number = int.into();
+    println!("A second number is {:?}", num);
+
+    // TryFrom
+
+    assert_eq!(EvenNumber::try_from(8), Ok(EvenNumber(8)));
+    assert_eq!(EvenNumber::try_from(5), Err(()));
+
+    // TryInto
+
+    let result: Result<EvenNumber, ()> = 8i32.try_into();
+    assert_eq!(result, Ok(EvenNumber(8)));
+    let result: Result<EvenNumber, ()> = 5i32.try_into();
+    assert_eq!(result, Err(()));
+
+    let circle = Circle { radius: 6 };
+    println!("{}", circle.to_string());
+
+    let parsed: i32 = "5".parse().unwrap();
+    let turbo_parsed = "10".parse::<i32>().unwrap();
+
+    let sum = parsed + turbo_parsed;
+    println!("Sum: {:?}", sum)
+}
