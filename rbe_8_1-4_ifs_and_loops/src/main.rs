66956fc252263d8bdc0d80a0181aaf67365f5a7b
William Elder <curseyoumcduck@gmail.com>
2024-04-18 00:34:29 +1000

    breaks and continues

--- rbe_8_1-4_ifs_and_loops/src/main.rs
+++ rbe_8_1-4_ifs_and_loops/src/main.rs
@@ -23,4 +23,25 @@ fn main() {
         }; // all let bindings need a semicolon!
 
     println!("{} -> {}", n, big_n);
+
+    let mut count = 0u32;
+
+    println!("Let's count until infinity!");
+
+    // Infinite loop
+    loop {
+        count += 1;
+
+        if count == 3 {
+            println!("three!");
+            continue;
+        }
+        
+        println!("{}", count);
+
+        if count == 5 {
+            println!("Ok, that's enough!");
+            break;
+        }
+    }
 }
