3b4af2359b1c037ec08ffc8d3098090f448155ee
William Elder <curseyoumcduck@gmail.com>
2024-04-19 00:03:00 +1000

    working through loops, fun fun fun

--- rbe_8_1-4_ifs_and_loops/src/main.rs
+++ rbe_8_1-4_ifs_and_loops/src/main.rs
@@ -44,4 +44,75 @@ fn main() {
             break;
         }
     }
+
+    // Nesting and labels
+
+    'outer: loop {
+        println!("Entered the outer loop!");
+
+        'inner: loop {
+            println!("Entered the inner loop!");
+
+            // This would break only the iner loop
+            // break
+
+            // This breaks the outer loop
+            break 'outer;
+        }
+
+        println!("This point will never be reached");
+        // and not only will it never be reached,
+        // the rust compiler *warns* you of this!
+        // very impressive
+    }
+
+    println!("Exited the outer loop");
+
+    // and you can return results from loops by putting the variable after
+    // the break statement
+
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    assert_eq!(result, 20);
+
+    // getting used to while with a fizzbuzz example
+
+    let mut n = 1;
+
+    while n < 101 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+
+        n += 1;
+    }
+
+    // and now for something completely different
+
+    for a in 1..101 { // alternatively for a in 1..=100
+        if a % 15 == 0 {
+            println!("fizzbuzz");
+        } else if a % 3 == 0 {
+            println!("fizz");
+        } else if a % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", a);
+        }
+    }
+
 }
