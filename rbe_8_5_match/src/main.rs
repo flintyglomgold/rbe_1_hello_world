e58ea337949760fbaa2e51c15f60078dbbb343fd
William Elder <curseyoumcduck@gmail.com>
2024-04-20 10:33:52 +1000

    Learning about destructuring arrays into slices in match statements.

--- rbe_8_5_match/src/main.rs
+++ rbe_8_5_match/src/main.rs
@@ -40,4 +40,31 @@ fn main() {
         _          => println!("It doesn't matter what they are."),
     }
 
+    // destructuring arrays
+
+    let array = [2, -2, 6];
+
+    match array {
+        [0, second, third] =>
+            println!("array[0] = 0, array[1] = {}, and array[2] = {}", second, third
+        ),
+
+        [1, _, third]      =>
+            println!("array[0] = 1, array[2] = {}, and array[1] was ignored", third
+        ),
+
+        [-1, second, ..]   =>
+            println!("array[0] = -1, array[1] = {}, and all others were ignored", second
+        ),
+
+        [3, second, tail @ ..] =>
+            println!("array[0] = 3, array[1] = {}, and the other elements were {:?}",
+                second, tail
+        ),
+
+        [first, middle @ .., last] =>
+            println!("array[0] = {}, middle = {:?}, and the last element is {}",
+                first, middle, last
+        ),
+    }
 }
